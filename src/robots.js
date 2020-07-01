af5340a8a97ffc6a2dc35fcf1d40ae05e4c20f80
Jon Haase <haase1020@gmail.com>
2020-07-01 14:13:06 -0400

    add cards and robot object

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Noel Haase',
+    username: 'noel',
+    email: 'noel@gmail.com',
+  },
+  {
+    id: 2,
+    name: 'Caleb Haase',
+    username: 'caleb',
+    email: 'caleb@gmail.com',
+  },
+  {
+    id: 3,
+    name: 'Hope Haase',
+    username: 'hope',
+    email: 'hope@gmail.com',
+  },
+  {
+    id: 4,
+    name: 'Timothy Haase',
+    username: 'timothy',
+    email: 'timothy@gmail.com',
+  },
+  {
+    id: 5,
+    name: 'Mandi Haase',
+    username: 'mandi',
+    email: 'mandi@gmail.com',
+  },
+  {
+    id: 6,
+    name: 'Jon Haase',
+    username: 'jon',
+    email: 'jon@gmail.com',
+  },
+];
