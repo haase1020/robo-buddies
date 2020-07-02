326431812ab9510b840751159d976d5a82277055
Jon Haase <haase1020@gmail.com>
2020-07-02 05:40:28 -0400

    Updates

---
+++ precache-manifest.e51a666283bf3bfeece1047e9acc08ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f763c83f0e98be94a5851b31cc1f9d",
+    "url": "/robo-buddies/index.html"
+  },
+  {
+    "revision": "ca8b8eb6a6c25498b43f",
+    "url": "/robo-buddies/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3ab2dfb60b5ba5ab97f1",
+    "url": "/robo-buddies/static/css/main.08d2a30a.chunk.css"
+  },
+  {
+    "revision": "ca8b8eb6a6c25498b43f",
+    "url": "/robo-buddies/static/js/2.deac2485.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-buddies/static/js/2.deac2485.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ab2dfb60b5ba5ab97f1",
+    "url": "/robo-buddies/static/js/main.fefc5e34.chunk.js"
+  },
+  {
+    "revision": "129cafc2819f0dca4d20",
+    "url": "/robo-buddies/static/js/runtime-main.46c2ede2.js"
+  }
+]);
